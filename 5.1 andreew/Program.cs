c393d5feb584816686ce8c632ed382037c8064ba
tihon <tihon@DESKTOP-ACP6SLU>
2021-12-20 11:14:27 +0200

    Добавьте файлы проекта.

---
+++ 5.1 andreew/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1_andreew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое предложение:");
+            string str = Console.ReadLine();
+            string s1 = str.Substring(0, str.IndexOf(' '));
+            string s2 = str.Substring(str.LastIndexOf(' ') + 1, str.Length - str.LastIndexOf(' ') - 1);
+            str = str.Remove(0, str.IndexOf(' '));
+            str = str.Remove(str.LastIndexOf(' ') + 1, str.Length - str.LastIndexOf(' ') - 1);
+            str = s2 + str + s1;
+            Console.WriteLine(str);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
